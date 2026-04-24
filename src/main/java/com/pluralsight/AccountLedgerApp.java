b7624e31608f95f89fb7489ad255d1b6a432ea50
kadillon21 <kadillon21@gmail.com>
2026-04-24 16:14:41 -0500

    promptForInt method created

--- src/main/java/com/pluralsight/AccountLedgerApp.java
+++ src/main/java/com/pluralsight/AccountLedgerApp.java
@@ -4,7 +4,6 @@ public class AccountLedgerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(UserInput.promptForDouble("Enter Double: ", 0, 10));
 
     }
 }
--- src/main/java/com/pluralsight/UserInput.java
+++ src/main/java/com/pluralsight/UserInput.java
@@ -10,6 +10,34 @@ public class UserInput {
 
     }
 
+    public static int promptForInt(String prompt, int min, int max) {
+        int input = 0;
+        boolean inputValid = false;
+        System.out.print(prompt);
+
+        while (!inputValid) {
+            if (scanner.hasNextInt()) {
+                input = scanner.nextInt();
+                if (input >= min && input <= max) {
+                    inputValid = true;
+                } else if (input <= min) {
+                    System.out.print(ColorUtilities.ERROR);
+                    System.out.println(" The valid you entered was too little...");
+                    System.out.print(prompt);
+                } else {
+                    System.out.print(ColorUtilities.ERROR);
+                    System.out.println(" The valid you entered was too high...");
+                    System.out.print(prompt);
+                }
+            } else {
+                System.out.print(ColorUtilities.ERROR);
+                System.out.println("You did not provide a valid input try again...");
+                System.out.println(prompt);
+            }
+        }
+        return input;
+    }
+
     public static double promptForDouble(String prompt, double min, double max) {
         double input = 0;
         boolean inputValid = false;
@@ -37,4 +65,6 @@ public class UserInput {
         }
         return input;
     }
+
+
 }
