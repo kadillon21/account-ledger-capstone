f46e6d5d5ffc53a701529a4430be140e3e74ff65
kadillon21 <kadillon21@gmail.com>
2026-04-24 16:09:11 -0500

    AccountLedgerApp.java created ColorUtilities.java imported Ledger.java created Menus.java created Reports.java created Transaction.java created UserInput.java created Added promptForDoubleMethod to UserInput.java

---
+++ src/main/java/com/pluralsight/AccountLedgerApp.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+public class AccountLedgerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(UserInput.promptForDouble("Enter Double: ", 0, 10));
+
+    }
+}
---
+++ src/main/java/com/pluralsight/UserInput.java
@@ -0,0 +1,40 @@
+package com.pluralsight;
+
+import java.awt.*;
+import java.util.Scanner;
+
+public class UserInput {
+    static Scanner scanner = new Scanner(System.in);
+
+    public UserInput(){
+
+    }
+
+    public static double promptForDouble(String prompt, double min, double max) {
+        double input = 0;
+        boolean inputValid = false;
+        System.out.print(prompt);
+
+        while (!inputValid) {
+            if (scanner.hasNextDouble()) {
+                input = scanner.nextDouble();
+                if (input >= min && input <= max) {
+                    inputValid = true;
+                } else if (input <= min) {
+                    System.out.print(ColorUtilities.ERROR);
+                    System.out.println(" The valid you entered was too little...");
+                    System.out.print(prompt);
+                } else {
+                    System.out.print(ColorUtilities.ERROR);
+                    System.out.println(" The valid you entered was too high...");
+                    System.out.print(prompt);
+                }
+            } else {
+                System.out.print(ColorUtilities.ERROR);
+                System.out.println("You did not provide a valid input try again...");
+                System.out.println(prompt);
+            }
+        }
+        return input;
+    }
+}
