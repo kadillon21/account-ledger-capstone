a57a2e942c0b0e0832429a295e99a48aee75d009
kadillon21 <kadillon21@gmail.com>
2026-04-25 22:15:35 -0500

    Main Menu, Ledger Menu, and ReportsMenu all have been created

--- src/main/java/com/pluralsight/Menus.java
+++ src/main/java/com/pluralsight/Menus.java
@@ -1,4 +1,50 @@
 package com.pluralsight;
 
 public class Menus {
+
+    public static void mainMenu() {
+        System.out.println();
+        System.out.println("╔══════════════════════════════╗");
+        System.out.println("║     ACCOUNTING LEDGER        ║");
+        System.out.println("╠══════════════════════════════╣");
+        System.out.println("║                              ║");
+        System.out.println("║  D)  Add Deposit             ║");
+        System.out.println("║  P)  Make Payment (Debit)    ║");
+        System.out.println("║  L)  Ledger                  ║");
+        System.out.println("║  X)  Exit                    ║");
+        System.out.println("║                              ║");
+        System.out.println("╚══════════════════════════════╝");
+    }
+
+    public static void LedgerMenu() {
+        System.out.println();
+        System.out.println("╔══════════════════════════════╗");
+        System.out.println("║          LEDGER              ║");
+        System.out.println("╠══════════════════════════════╣");
+        System.out.println("║                              ║");
+        System.out.println("║  A)  All Entries             ║");
+        System.out.println("║  D)  Deposits                ║");
+        System.out.println("║  P)  Payments                ║");
+        System.out.println("║  R)  Reports                 ║");
+        System.out.println("║  H)  Home                    ║");
+        System.out.println("║                              ║");
+        System.out.println("╚══════════════════════════════╝");
+    }
+
+    public static void ReportsMenu() {
+        System.out.println();
+        System.out.println("╔══════════════════════════════╗");
+        System.out.println("║          REPORTS             ║");
+        System.out.println("╠══════════════════════════════╣");
+        System.out.println("║                              ║");
+        System.out.println("║  1)  Month To Date           ║");
+        System.out.println("║  2)  Previous Month          ║");
+        System.out.println("║  3)  Year To Date            ║");
+        System.out.println("║  4)  Previous Year           ║");
+        System.out.println("║  5)  Search by Vendor        ║");
+        System.out.println("║  0)  Back                    ║");
+        System.out.println("║                              ║");
+        System.out.println("╚══════════════════════════════╝");
+    }
+
 }
