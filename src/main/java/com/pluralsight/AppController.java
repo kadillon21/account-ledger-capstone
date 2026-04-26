0aa51c8f0e496414f42a481d35f1d347379c62e0
kadillon21 <kadillon21@gmail.com>
2026-04-25 23:07:38 -0500

    AppController Class Created

---
+++ src/main/java/com/pluralsight/AppController.java
@@ -0,0 +1,28 @@
+package com.pluralsight;
+
+public class AppController {
+
+    public void run(){
+
+    }
+
+    private void handleHomeScreen(){
+
+    }
+
+    private void handleLedgerScreen(){
+
+    }
+
+    private void handleReportsScreen(){
+
+    }
+
+    private void addDeposit(){
+
+    }
+
+    private void makePayment(){
+
+    }
+}
