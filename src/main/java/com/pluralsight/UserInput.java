b30b118d9314a02b3bb4821ca79fab7085b53d27
kadillon21 <kadillon21@gmail.com>
2026-04-27 12:05:14 -0500

    Added another promptForDouble method that only requires a prompt and min and not a max.

--- src/main/java/com/pluralsight/UserInput.java
+++ src/main/java/com/pluralsight/UserInput.java
@@ -1,6 +1,7 @@
 package com.pluralsight;
 
 import java.awt.*;
+
 import java.util.Scanner;
 
 public class UserInput {
@@ -66,6 +67,30 @@ public class UserInput {
         return input;
     }
 
+    public static double promptForDouble(String prompt, double min) {
+        double input = 0;
+        boolean inputValid = false;
+        System.out.print(prompt);
+
+        while (!inputValid) {
+            if (scanner.hasNextDouble()) {
+                input = scanner.nextDouble();
+                if (input >= min) {
+                    inputValid = true;
+                } else {
+                    System.out.print(ColorUtilities.ERROR);
+                    System.out.println(" The valid you entered was too little...");
+                    System.out.print(prompt);
+                }
+            } else {
+                System.out.print(ColorUtilities.ERROR);
+                System.out.println("You did not provide a valid input try again...");
+                System.out.println(prompt);
+            }
+        }
+        return input;
+    }
+
     public static String promptForString(String prompt, String comparative) {
         String input = "";
         boolean inputValid = false;
@@ -84,6 +109,57 @@ public class UserInput {
         }
         return input;
     }
+
+    public static String promptForString(String prompt) {
+        System.out.print(prompt);
+        return scanner.nextLine();
+    }
+    
+    public static char promptForChar(String prompt){
+        char returnValue = 0;
+        boolean inputValid = false;
+        System.out.print(prompt);
+
+        while (!inputValid) {
+            String input = scanner.nextLine();
+            if (input.length() == 1) {
+                returnValue = input.charAt(0);
+                inputValid = true;
+            } else {
+                System.out.print(ColorUtilities.ERROR);
+                System.out.println(" You did not enter a valid menu option");
+                System.out.println(prompt);
+            }
+        }
+        return returnValue;
+    }
+
+
+
+/*
+    public static char promptForChar(String prompt, String[] alternatives){
+        char returnValue = 0;
+        boolean inputValid = false;
+        System.out.print(prompt);
+        String input = scanner.nextLine();
+
+        while (!inputValid) {
+            if (input.length() == 1) {
+                returnValue = input.charAt(0);
+                inputValid = true;
+            }
+            else {
+                for(String alt : alternatives){
+
+                }
+                System.out.print(ColorUtilities.ERROR);
+                System.out.println(" You did not enter a valid menu option");
+                System.out.println(prompt);
+            }
+        }
+        return returnValue;
+    }
+*/
 
 
 }
