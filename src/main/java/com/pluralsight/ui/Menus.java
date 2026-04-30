6464bd89f708467914d0ee3a170bb8e86eb77a24
kadillon21 <kadillon21@gmail.com>
2026-04-30 12:47:48 -0500

    Comments have been added to Menus.java

--- src/main/java/com/pluralsight/ui/Menus.java
+++ src/main/java/com/pluralsight/ui/Menus.java
@@ -7,20 +7,24 @@ import com.pluralsight.util.ConsoleUtilities;
 import com.pluralsight.util.UserInput;
 
 import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Menus {
 
+    // Displays dashboard
     public static void displayDashboard(Ledger ledger) {
-        String B  = ConsoleUtilities.BORDER;
-        String R  = ConsoleUtilities.RESET;
+        String B = ConsoleUtilities.BORDER;
+        String R = ConsoleUtilities.RESET;
 
         double balance = ledger.getBalance();
         String balanceColor = balance >= 0 ? ConsoleUtilities.SUCCESS : ConsoleUtilities.DANGER;
+        // formats money properly
         NumberFormat money = NumberFormat.getCurrencyInstance();
 
         System.out.println();
-        System.out.println(B + "╔═ QUICK STATS " + "═══════════════════════════════════════"+ "╗");
+        System.out.println(B + "╔═ QUICK STATS " + "═══════════════════════════════════════" + "╗");
         System.out.printf(B + "║  Balance:        " + balanceColor + "%-35s" + B + "║%n", money.format(balance));
         System.out.printf(B + "║  MTD Income:     " + ConsoleUtilities.GREEN + "%-35s" + B + "║%n", money.format(ledger.getMonthToDateIncome()));
         System.out.printf(B + "║  MTD Expenses:   " + ConsoleUtilities.RED + "%-35s" + B + "║%n", money.format(ledger.getMonthToDateExpenses()));
@@ -28,12 +32,13 @@ public class Menus {
         System.out.println(B + "╚" + "═════════════════════════════════════════════════════" + "╝" + R);
     }
 
+    // Displays main menu
     public static void mainMenu() {
-        String B  = ConsoleUtilities.BORDER;
-        String A  = ConsoleUtilities.ACCENT;
+        String B = ConsoleUtilities.BORDER;
+        String A = ConsoleUtilities.ACCENT;
         String Bo = ConsoleUtilities.BOLD;
-        String U  = ConsoleUtilities.UNDERLINE;
-        String R  = ConsoleUtilities.RESET;
+        String U = ConsoleUtilities.UNDERLINE;
+        String R = ConsoleUtilities.RESET;
 
         System.out.println();
         System.out.println(B + "╔══════════════════════════════╗");
@@ -48,12 +53,13 @@ public class Menus {
         System.out.println(B + "╚══════════════════════════════╝" + R);
     }
 
+    // Display ledger menu
     public static void ledgerMenu() {
-        String B  = ConsoleUtilities.BORDER;
-        String A  = ConsoleUtilities.ACCENT;
+        String B = ConsoleUtilities.BORDER;
+        String A = ConsoleUtilities.ACCENT;
         String Bo = ConsoleUtilities.BOLD;
-        String U  = ConsoleUtilities.UNDERLINE;
-        String R  = ConsoleUtilities.RESET;
+        String U = ConsoleUtilities.UNDERLINE;
+        String R = ConsoleUtilities.RESET;
 
         System.out.println();
         System.out.println(B + "╔══════════════════════════════╗");
@@ -69,12 +75,13 @@ public class Menus {
         System.out.println(B + "╚══════════════════════════════╝" + R);
     }
 
+    // Displays reports menu
     public static void reportsMenu() {
-        String B  = ConsoleUtilities.BORDER;
-        String A  = ConsoleUtilities.ACCENT;
+        String B = ConsoleUtilities.BORDER;
+        String A = ConsoleUtilities.ACCENT;
         String Bo = ConsoleUtilities.BOLD;
-        String U  = ConsoleUtilities.UNDERLINE;
-        String R  = ConsoleUtilities.RESET;
+        String U = ConsoleUtilities.UNDERLINE;
+        String R = ConsoleUtilities.RESET;
 
         System.out.println();
         System.out.println(B + "╔══════════════════════════════╗");
@@ -92,12 +99,13 @@ public class Menus {
         System.out.println(B + "╚══════════════════════════════╝" + R);
     }
 
+    // displays transaction type menu
     public static void transTypeMenu() {
-        String B  = ConsoleUtilities.BORDER;
-        String A  = ConsoleUtilities.ACCENT;
+        String B = ConsoleUtilities.BORDER;
+        String A = ConsoleUtilities.ACCENT;
         String Bo = ConsoleUtilities.BOLD;
-        String U  = ConsoleUtilities.UNDERLINE;
-        String R  = ConsoleUtilities.RESET;
+        String U = ConsoleUtilities.UNDERLINE;
+        String R = ConsoleUtilities.RESET;
 
         System.out.println();
         System.out.println(B + "╔══════════════════════════════╗");
@@ -111,12 +119,13 @@ public class Menus {
         System.out.println(B + "╚══════════════════════════════╝" + R);
     }
 
+    // displays edit values menu
     public static void editValuesMenu() {
-        String B  = ConsoleUtilities.BORDER;
-        String A  = ConsoleUtilities.ACCENT;
+        String B = ConsoleUtilities.BORDER;
+        String A = ConsoleUtilities.ACCENT;
         String Bo = ConsoleUtilities.BOLD;
-        String U  = ConsoleUtilities.UNDERLINE;
-        String R  = ConsoleUtilities.RESET;
+        String U = ConsoleUtilities.UNDERLINE;
+        String R = ConsoleUtilities.RESET;
 
         System.out.println();
         System.out.println(B + "╔══════════════════════════════╗");
@@ -134,12 +143,15 @@ public class Menus {
         System.out.println(B + "╚══════════════════════════════╝" + R);
     }
 
+    // Displays custom search options
     public static void customSearchOptions(SearchCriteria criteria) {
         String B = ConsoleUtilities.BORDER;
         String A = ConsoleUtilities.ACCENT;
         String Bo = ConsoleUtilities.BOLD;
         String U = ConsoleUtilities.UNDERLINE;
         String R = ConsoleUtilities.RESET;
+
+        // formats doubles into a proper money format
         NumberFormat money = NumberFormat.getCurrencyInstance();
 
         System.out.println();
@@ -147,13 +159,14 @@ public class Menus {
         System.out.println(B + "║     " + Bo + U + R + "                  Custom Search Menu                   " + R + B + "║");
         System.out.println(B + "╠════════════════════════════════════════════════════════════╣");
         System.out.println(B + "║                                                            ║");
-        System.out.printf( B + "║  Start Date: " + A + "%-46s" + B + "║%n", displayValue(criteria.getStartDate()));
-        System.out.printf( B + "║  End Date: " + A + "%-48s" + B + "║%n", displayValue(criteria.getEndDate()));
-        System.out.printf( B + "║  Description: " + A + "%-45s" + B + "║%n", displayValue(criteria.getDescription()));
-        System.out.printf( B + "║  Vendor: " + A + "%-50s" + B + "║%n",displayValue(criteria.getVendor()));
-        System.out.printf( B + "║  Min Amount: " + A + "%-46s" + B + "║%n", money.format(criteria.getMinAmount()));
-        System.out.printf( B + "║  Max Amount: " + A + "%-46s" + B + "║%n", money.format(criteria.getMaxAmount()));
-        System.out.printf( B + "║  Transaction Type: " + A + "%-40s" + B + "║%n", displayValue(criteria.getTransType()));
+        System.out.printf(B + "║  Start Date: " + A + "%-46s" + B + "║%n", displayValue(criteria.getStartDate()));
+        System.out.printf(B + "║  End Date: " + A + "%-48s" + B + "║%n", displayValue(criteria.getEndDate()));
+        System.out.printf(B + "║  Description: " + A + "%-45s" + B + "║%n", displayValue(criteria.getDescription()));
+        System.out.printf(B + "║  Vendor: " + A + "%-50s" + B + "║%n", displayValue(criteria.getVendor()));
+        System.out.printf(B + "║  Min Amount: " + A + "%-46s" + B + "║%n", money.format(criteria.getMinAmount()));
+        System.out.printf(B + "║  Max Amount: " + A + "%-46s" + B + "║%n", money.format(criteria.getMaxAmount()));
+        System.out.printf(B + "║  Transaction Type: " + A + "%-40s" + B + "║%n", displayValue(criteria.getTransType()));
+        System.out.println(B + "║                                                            ║");
         System.out.println(B + "║  " + A + "E" + R + ")  Edit Values                                           " + B + "║");
         System.out.println(B + "║  " + A + "S" + R + ")  Search                                                " + B + "║");
         System.out.println(B + "║  " + A + "X" + R + ")  Cancel                                                " + B + "║");
@@ -161,12 +174,16 @@ public class Menus {
         System.out.println(B + "╚════════════════════════════════════════════════════════════╝" + R);
     }
 
+    // displays transactions based of criteria
     public static void displayTransactions(List<Transaction> transactions) {
         String B = ConsoleUtilities.BORDER;
         String A = ConsoleUtilities.ACCENT;
         String M = ConsoleUtilities.MUTED;
         String R = ConsoleUtilities.RESET;
         NumberFormat money = NumberFormat.getCurrencyInstance();
+
+        List<Transaction> sorted = new ArrayList<>(transactions);
+        Collections.reverse(sorted);
 
         if (transactions.isEmpty()) {
             System.out.println(ConsoleUtilities.WARNING + "No transactions to display.");
@@ -178,9 +195,10 @@ public class Menus {
         int totalPages = (int) Math.ceil((double) transactions.size() / pageSize);
 
         boolean viewing = true;
-        while (viewing) {
+        while (viewing) { // Loop to keep viewing so user can select pages
             ConsoleUtilities.spin("Loading...", 1000);
 
+            // Header for transactions
             System.out.println(B + "\n╔══════════════════════════════════════════════════════════════════════════════════════════════════════╗");
             System.out.println(B + "║                                         ACCOUNT LEDGER                                               ║");
             System.out.println(B + "╠════════════╦══════════╦════════════════════════════════╦═══════════════════════════╦═════════════════╣");
@@ -194,6 +212,7 @@ public class Menus {
 
                 String amountColor = transaction.isDeposit() ? ConsoleUtilities.SUCCESS : ConsoleUtilities.DANGER;
 
+                // Format for each line to print transaction
                 System.out.printf(B + "║" + R + " %-11s" + B + "║" + M + " %-9s" + B + "║" + R + " %-30s " + B + "║" + R + " %-25s " + B + "║" + amountColor + "%16s" + B + " ║%n",
                         transaction.getDate(),
                         transaction.getTime(),
@@ -203,6 +222,7 @@ public class Menus {
                 );
             }
 
+            // used to ensure border formatting is never off if page numbers have multiple digits
             int digits = String.valueOf(transactions.size()).length();
             String spacer = switch (digits) {
                 case 1 -> "%79s║%n";
@@ -211,6 +231,8 @@ public class Menus {
                 default -> "%76s║%n";
             };
 
+
+            // used to ensure border formatting is never off if page numbers have multiple digits
             int digits2 = String.valueOf(totalPages).length();
             digits2 += String.valueOf(currentPage + 1).length();
             String spacer2 = switch (digits2) {
@@ -221,13 +243,13 @@ public class Menus {
                 default -> "%85s║%n";
             };
 
-
+            // Footer that displays page number, total transactions, and number of pages
             System.out.println(B + "╠════════════╩══════════╩════════════════════════════════╩═══════════════════════════╩═════════════════╣");
             System.out.printf(B + "║  " + A + "%d total transactions " + B + spacer, transactions.size(), "");
-            System.out.printf(B + "║  " + A + "Page %d of %d" + B + spacer2 , currentPage + 1, totalPages, "");
+            System.out.printf(B + "║  " + A + "Page %d of %d" + B + spacer2, currentPage + 1, totalPages, "");
             System.out.println(B + "╚══════════════════════════════════════════════════════════════════════════════════════════════════════╝" + R);
 
-
+            // Menu to prompt user if they want to go to next page or continue.
             switch (UserInput.promptForChar("[N]ext  [P]rev  [Q]uit: ", "NPQ")) {
                 case 'N':
                     if (currentPage < totalPages - 1) currentPage++;
@@ -244,11 +266,13 @@ public class Menus {
 
     }
 
+    // Method to shorten string by given amount (maxLen) and adds a ... at the end to indicate its shortened
     public static String truncate(String s, int maxLen) {
         if (s.length() <= maxLen) return s;
         return s.substring(0, maxLen - 3) + "... ";
     }
 
+    // used in custom search to turn empty and null values to display the string "(any)"
     private static String displayValue(Object value) {
         if (value == null) return "(any)";
         if (value.toString().isEmpty()) return "(any)";
