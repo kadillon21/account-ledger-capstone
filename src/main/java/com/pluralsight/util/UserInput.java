e27b3c4b6852945857ae0aad0e91fe877d7ab302
kadillon21 <kadillon21@gmail.com>
2026-04-30 10:49:28 -0500

    Comments have been added to UserInput.java

--- src/main/java/com/pluralsight/util/UserInput.java
+++ src/main/java/com/pluralsight/util/UserInput.java
@@ -4,13 +4,16 @@ import java.time.LocalDate;
 import java.util.Scanner;
 
 public class UserInput {
-    static Scanner scanner = new Scanner(System.in);
-    public static final String arrow = ConsoleUtilities.ACCENT + "❯ " + ConsoleUtilities.RESET;
+    static Scanner scanner = new Scanner(System.in); // Only instance of scanner being called in program
+    public static final String arrow = ConsoleUtilities.ACCENT + "❯ " + ConsoleUtilities.RESET; // Arrow key used
 
     public UserInput(){
 
     }
 
+    /* This method prompts a user for string, uses a try catch to attempt
+       to parse string into an int and loops until a valid input is given.
+     */
     public static int promptForInt(String prompt, int min, int max) {
         String input;
         int value = 0;
@@ -19,9 +22,11 @@ public class UserInput {
         System.out.print(prompt);
 
         while (!inputValid) {
-            input = scanner.nextLine();
+            input = scanner.nextLine(); // Takes in user input as a String
             try{
-                value = Integer.parseInt(input);
+                value = Integer.parseInt(input); // tries to parse string into an int
+                // if statements verify if parsed int meets the required minimums and maximum
+                // Keeps looping until inputValid is = true
                 if (value >= min && value <= max) {
                     inputValid = true;
                 } else if (value <= min) {
@@ -33,7 +38,7 @@ public class UserInput {
                     System.out.println(" The value you entered was too high...");
                     System.out.print(prompt);
                 }
-            } catch(Exception e) {
+            } catch(Exception e) { // Tells user that the input provided was not valid.
                 System.out.print(ConsoleUtilities.ERROR);
                 System.out.println("You did not provide a valid input try again...");
                 System.out.print(prompt);
@@ -42,6 +47,7 @@ public class UserInput {
         return value;
     }
 
+    // Unused method, same as above but just for doubles instead of integers
     public static double promptForDouble(String prompt, double min, double max) {
         String input;
         double value = 0;
@@ -73,6 +79,7 @@ public class UserInput {
         return value;
     }
 
+    // Prompts user for double but only takes in a minimum as a parameter
     public static double promptForDouble(String prompt, double min) {
         String input;
         double value = 0;
@@ -101,32 +108,14 @@ public class UserInput {
         return value;
     }
 
-    public static String promptForString(String prompt, String comparative) {
-        String input = "";
-        boolean inputValid = false;
-        prompt += arrow;
-        System.out.print(prompt);
-
-        while (!inputValid) {
-            input = scanner.nextLine();
-            if (input.equalsIgnoreCase(comparative)) {
-                inputValid = true;
-            } else {
-                System.out.print(ConsoleUtilities.ERROR);
-                System.out.println(" You did not enter a valid menu option");
-                System.out.print(prompt);
-            }
-
-        }
-        return input;
-    }
-
+    // Prompts user for String returns the users input.
     public static String promptForString(String prompt) {
         prompt += arrow;
         System.out.print(prompt);
         return scanner.nextLine();
     }
-    
+
+    // Prompts user for a String and validates that an actual character was provided
     public static char promptForChar(String prompt){
         char returnValue = 0;
         boolean inputValid = false;
@@ -135,10 +124,10 @@ public class UserInput {
 
         while (!inputValid) {
             String input = scanner.nextLine().toUpperCase();
-            if (input.length() == 1) {
+            if (input.length() == 1) { // if the length of the string is equal to one its consider a char
                 returnValue = input.charAt(0);
                 inputValid = true;
-            } else {
+            } else { // if the input is less than 1 or greater than 1 then it gives the user an error messages and loops back over.
                 System.out.println(ConsoleUtilities.ERROR + "You did not enter a valid menu option");
                 System.out.println(prompt);
             }
@@ -146,24 +135,25 @@ public class UserInput {
         return returnValue;
     }
 
+    // Uses the other promptForChar method to prompt for a char and takes valid in a string of valid chars to compare to
     public static char promptForChar(String prompt, String validChars) {
         String valid = validChars.toUpperCase();
         while (true) {
             char input = promptForChar(prompt);
-            if (valid.indexOf(input) >= 0) {
-                return input;
+            if (valid.indexOf(input) >= 0) { // Checks if the CHAR provided is found in the String of chars provided.
+                return input; // returns char
             }
             System.out.println(ConsoleUtilities.ERROR + " Invalid choice... Try again");
         }
     }
 
 
-
+    // Prompts user for a date and takes it in as a String and attempts to parse it into a LocalDate variable and returns that variable
     public static LocalDate promptForDate(String prompt){
         boolean inputValid = false;
         prompt += arrow;
         System.out.print(prompt);
-        LocalDate date = LocalDate.now();
+        LocalDate date = LocalDate.now(); // Creates local date variable
 
         while (!inputValid){
             String input = scanner.nextLine();
@@ -179,7 +169,7 @@ public class UserInput {
             }
         }
 
-        return date;
+        return date; // returns users input as a LocalDate variable
     }
 
 }
